edd2ecea5d83339ad217b55f167162e0822c7049
Nick Turner <nturner@enova.com>
2019-03-12 08:53:02 -0500

    make list

--- public/pages.js
+++ public/pages.js
@@ -5,15 +5,20 @@ $(document).ready(function() {
         url: "https://magmiler.github.io/races/",
         dataType: "json"
       }).done(function(result) {
-        var dataContainer = $("#data-container");
-        let dstring = "Races:<br>"
+        var eventList = $("#event-list");
+        let dstring = ""
         for (i = 0; i < result['Races'].length; ++i) {
           let title = result['Races'][i]['title'];
           let distance = result['Races'][i]['distance'];
           let date = result['Races'][i]['date'];
-
-          dstring = dstring + title + ": Distance: " + distance + ", Date: " + date + "<br>"
+          dstring = dstring + '<li><time><span class="day">' + date + '</span>'
+          dstring = dstring + '<span class="month">' + date + '</span></time>'
+          dstring = dstring + '<div class="info"><h2 class="title">' + title + '</h2>'
+          dstring = dstring + '<p class="desc"><i class="fas fa-map-signs event-list-icon"></i>' + location
+          dstring = dstring + '<i class="fas fa-map-signs event-list-icon"></i>' + date
+          dstring = dstring + '<br><i class="fas fa-road event-list-icon"></i>' + distance
+          dstring = dstring + '</p></div></li>'
         }
-        dataContainer.html(dstring);
+        eventList.html(dstring);
       })
 })
