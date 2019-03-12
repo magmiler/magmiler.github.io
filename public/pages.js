d287874dc054564273385f9cb8bd1b2c55b88979
Nick Turner <nturner@enova.com>
2019-03-12 09:23:13 -0500

    fix formating

--- public/pages.js
+++ public/pages.js
@@ -11,11 +11,12 @@ $(document).ready(function() {
           let title = result['Races'][i]['title'];
           let distance = result['Races'][i]['distance'];
           let date = result['Races'][i]['date'];
+          let location = result['Races'][i]['location'];
           dstring = dstring + '<li><time><span class="day">' + date + '</span>'
           dstring = dstring + '<span class="month">' + date + '</span></time>'
           dstring = dstring + '<div class="info"><h2 class="title">' + title + '</h2>'
           dstring = dstring + '<p class="desc"><i class="fas fa-map-signs event-list-icon"></i>' + location
-          dstring = dstring + '<i class="fas fa-map-signs event-list-icon"></i>' + date
+          dstring = dstring + '<i class="far fa-clock event-list-icon"></i>' + date
           dstring = dstring + '<br><i class="fas fa-road event-list-icon"></i>' + distance
           dstring = dstring + '</p></div></li>'
         }
