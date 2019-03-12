8c59bf6d6fe93f8295220fe9fb1f75889d10bd68
Nick Turner <nturner@enova.com>
2019-03-11 21:29:44 -0500

    fix jquery

--- public/pages.js
+++ public/pages.js
@@ -1,18 +1,17 @@
-// find elements
-var dataContainer = $("#data-container");
 
 // handle click and add class
 $(document).ready(function() {
+      $.ajax({
+        url: "https://magmiler.github.io/races/",
+        dataType: "json"
+      }).done(function(result) {
+        var dataContainer = $("#data-container");
 
-  $.ajax({
-    url: "https://magmiler.github.io/races/",
-    dataType: "json"
-  }).done(function(result) {
-    let title = result['Races'][1]['title'];
-    let distance = result['Races'][1]['distance'];
-    let date = result['Races'][1]['date'];
+        let title = result['Races'][1]['title'];
+        let distance = result['Races'][1]['distance'];
+        let date = result['Races'][1]['date'];
 
-    let dstring = "Race: " + title + ", Distance: " + distance + ", Date: " + date;
-    dataContainer.text(dstring);
-  });
-});
+        let dstring = "Race: " + title + ", Distance: " + distance + ", Date: " + date;
+        dataContainer.text(dstring);
+      })
+})
