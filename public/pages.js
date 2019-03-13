1698c69ee723a977cd06e8522376c7d644d7c846
Nick Turner <nturner@enova.com>
2019-03-13 15:26:17 -0500

    update json

--- public/pages.js
+++ public/pages.js
@@ -4,11 +4,11 @@ $(document).ready(function() {
   $.getJSON("https://magmiler.github.io/races/", function(data){
     let dstring = ""
     $.each(data, function (index, race) {
-      dstring = dstring + '<li><time><span class="day">' + race.Date + '</span>'
-      dstring = dstring + '<span class="month">' + race.Date + '</span></time>'
+      dstring = dstring + '<li><time><span class="day">' + race.Day + '</span>'
+      dstring = dstring + '<span class="month">' + race.Month + '</span></time>'
       dstring = dstring + '<div class="info"><h2 class="title">' + race.Title + '</h2>'
       dstring = dstring + '<p class="desc"><i class="fas fa-map-signs event-list-icon"></i>' + race.Location
-      dstring = dstring + '<br><i class="far fa-clock event-list-icon"></i>' + race.Time
+      dstring = dstring + '<br><i class="far fa-clock event-list-icon"></i>' + race.Start
       dstring = dstring + '<br><i class="fas fa-road event-list-icon"></i>' + race.Distance
       dstring = dstring + '</p></div></li>'
     });
